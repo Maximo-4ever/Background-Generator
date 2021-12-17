81d9fdbc957f2f2e84760c721e3093807567eb0a
Maximo-4ever <maximilianocm06@gmail.com>
2021-12-16 21:58:27 -0600

    añade

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ const hexCode = document.querySelector(".card__hexcode");
 const btnGenerator = document.querySelector(".btn-generator");
 const btnCopy = document.querySelector(".btn-copy");
 
-const changeHexcolor = () => {
-  newHexcode = `rgb(
-    ${Math.round(Math.random() * 255)},
-    ${Math.round(Math.random() * 255)},
-    ${Math.round(Math.random() * 255)}
-  )`;
+btnGenerator.addEventListener("click", () => {
+  newHexcode = rgbToHex(
+    Math.round(Math.random() * 255),
+    Math.round(Math.random() * 255),
+    Math.round(Math.random() * 255)
+  );
   bgColor.style.background = newHexcode;
   hexCode.style.background = newHexcode;
-};
-
-btnGenerator.addEventListener("click", () => {
-  changeHexcolor();
+  hexCode.textContent = newHexcode;
 });
+btnCopy.addEventListener("click", () => {
+  navigator.clipboard.writeText(hexCode.textContent);
+});
+
+//Tranform RGB color to Hex color
+const componentToHex = (c) => {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+};
+const rgbToHex = (r, g, b) => {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+};
